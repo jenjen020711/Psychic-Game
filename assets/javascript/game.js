6ba46ce97fb92e06a6e499864c2d4a83d11448a5
jensweatmon <jenjenhuynh@gmail.com>
2018-02-27 09:59:26 -0500

    adjusted index file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,1 @@
-var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", 
-        "t", "u", "v", "w", "x", "y", "z"];
 
-        document.onkeyup = function() {
-            var userGuess = String.fromCharCode(event.keyCode)
-                toLowerCase();
-
-            console.log(userGuess); 
-
-            var computerGuess = options[Math.floor(Math.random()*options.length)];
-
-            console.log(computerGuess);
-
-        if (userGuess = computerGuess){
-            wins++;
-            alert("You Win!");        }
-        }
